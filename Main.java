7b84aab4af5dd2765e8b17300a1c14a4b8f814a3
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-17 21:18:28 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package days9;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    char c;
+
+    for(c = 'A'; c <= 'Z'; ++c)
+      System.out.print(c + " ");
+    }
+    
+}
